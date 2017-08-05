498e78935b3df7aee32436ddb6ce41e1476d1012
k0t3n <k0t3n.mail@gmail.com>
2017-08-05 14:23:26 +0300

    Add example template Local urls.py in navalny_people app

--- core/settings.py
+++ core/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'navalny_people/templates')]
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'navalny_people/templates'),)
