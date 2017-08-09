81ac57fd3bc666c1d0fbb8c34aa978b6bdeb16ee
kdelinx <kdelinx@gmail.com>
2017-08-09 12:55:45 +0300

    fuck yeah!

--- core/settings.py
+++ core/settings.py
@@ -1,17 +1,10 @@
 import os
+from core.settings_local import *
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '-)agk^!pxbcf9&krm@rr&)=&%0sh0rst!b5$fwvbkpt0w_+p3-'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-# Application definition
-
+ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     # Admin apps
     'suit',
@@ -25,7 +18,6 @@ INSTALLED_APPS = [
     # Project_apps
     'navalny_people',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -35,9 +27,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'core.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -51,26 +41,13 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
-    },
-]
-
-WSGI_APPLICATION = 'core.wsgi.application'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
-
+]
+WSGI_APPLICATION = 'core.wsgi.application'
 LANGUAGE_CODE = 'ru-RU'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/assets/'
