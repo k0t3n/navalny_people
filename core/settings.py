408e26a13edbc934b8c1d220cf1663cd8948d397
k0t3n <k0t3n.mail@gmail.com>
2017-08-13 04:36:23 +0300

    Add detail people page

--- core/settings.py
+++ core/settings.py
@@ -52,11 +52,12 @@ USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/assets/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-# )
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
+MEDIA_URL = '/media/'
 GOOGLEMAPS_KEY = 'AIzaSyCi3dmQDG8mz6Mb6w1FzVXk-OvYfUvZE04'
 VK_CLIENT_ID = '6093659'
 VK_SECRET_KEY = 'kTJuqF3GBO7UGtL4NlDJ'
